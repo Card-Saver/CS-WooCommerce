636305f602757e841e4fbb0b2249dc60b4862206
Card Saver <117294895+Card-Saver@users.noreply.github.com>
2022-12-02 15:10:56 +0000

    Updated for new version 6.x
    Compatible with version 6.x of Woocommerce and upto 6.x of Wordpress.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'gateway_title' => 'Card Saver',
+    'method_description' => 'Pay securely via Credit / Debit Card with Card Saver Gateway',
+    'default_merchant_id' => 'MMS ONLINE MID',
+    'default_secret' => 'Signiture Key',
+];
