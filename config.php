c2114818350a7862172e18b43de9051cc653c50a
Card Saver <117294895+Card-Saver@users.noreply.github.com>
2023-08-16 09:45:45 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
 
 return [
-	'gateway_title' => 'Card Saver',
-	'method_description' => 'Pay securely via Credit / Debit Card with Card Saver',
-	'default_merchant_id' => 'MMS MID',
-	'default_secret' => 'MMS Signiture Key',
-	'default_merchant_country_code' => '826',
+	'default' => [
+		'gateway_title' => 'Card Saver',
+		'method_description' => 'Pay securely via Credit / Debit Card with Card Saver',
+	],
+	'applepay' => [
+		'method_description' => 'Pay securely via Apple Pay with Card Saver',
+		'title' => 'Apple Pay',
+		'gateway_validation_available' => true,
+	],
+	'module' => [
+		'version' => '3.0.0'
+	]
 ];
